a0bf4f4bec25195c12384a578df578145786b1e3
developthinking <wutihome@outlook.com>
2017-11-08 11:41:38 +0800

    第二十四章：SpringBoot项目多模块打包与部署

---
+++ chapter24/bean/src/main/java/cn/wuti/chapter24/bean/UserBean.java
@@ -0,0 +1,29 @@
+package cn.wuti.chapter24.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2017/11/8.
+ */
+public class UserBean implements Serializable {
+    //用户名
+    private String name;
+    //密码
+    private String pwd;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
